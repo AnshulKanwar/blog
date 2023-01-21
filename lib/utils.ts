fc351ade61e5e3c0a4fdc54dd129e61893540dfc
Anshul Kanwar <4n5hu1@protonmail.com>
2023-01-21 11:25:52 +0530

    added util function

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,3 +7,10 @@ export const formatDate = (dateStr: string) => {
 export const capitalize = (str: string) => {
     return str.charAt(0).charAt(0).toUpperCase() + str.slice(1)
 }
+
+export const readingTime = (text: string) => {
+    const wpm = 200
+    const words = text.trim().split(/\s+/).length
+    const time = Math.ceil(words/wpm)
+    return time
+}
